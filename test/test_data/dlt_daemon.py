59e001cc7cde536859a41fad08986889508faee0
Fabrice Fontaine <fabrice.fontaine@orange.com>
2024-10-28 22:11:31 +0100

    feat(checker): add dlt-daemon (#4513)
    Fix #4457
    
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/dlt_daemon.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2024 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {"product": "dlt-daemon", "version": "2.18.0", "version_strings": ["2.18.0\nDLT_"]}
+]
+package_test_data = [
+    {
+        "url": "http://ftp.debian.org/debian/pool/main/d/dlt-daemon/",
+        "package_name": "dlt-daemon_2.18.0-1_amd64.deb",
+        "product": "dlt-daemon",
+        "version": "2.18.0",
+    },
+    {
+        "url": "http://ftp.debian.org/debian/pool/main/d/dlt-daemon/",
+        "package_name": "dlt-daemon_2.18.8-6_arm64.deb",
+        "product": "dlt-daemon",
+        "version": "2.18.8",
+    },
+]
