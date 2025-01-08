a8800da084c18f7600c4ab1ddb3ca5547fe18dda
Pratham Hole <prathamhole@gmail.com>
2025-01-09 02:41:07 +0530

    fix: Adds mismatch in package_data to resolve `ModuleNotFoundError` (#4669)
    * Fixes: #4655

---
+++ mismatch/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2025 Intel Corporation
+# SPDX-License-Identifier: GPL-3.0-or-later
+# This file is part of the CVE Binary Tool project.
+# It is used to initialize the mismatch module.
