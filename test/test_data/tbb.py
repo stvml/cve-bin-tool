d3d46b7bb6ca27082b329a915025e11888f68e2b
Frederik Carlier <frederik.carlier@keysight.com>
2025-02-20 01:29:52 +0100

    feat(checker): Add Thread Building Blocks (#4744)

---
+++ test/test_data/tbb.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2025 Keysight Technologies
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {
+        "product": "threading_building_blocks",
+        "version": "2019.0",
+        "version_strings": ["TBB: VERSION            2019.0"],
+    },
+    {
+        "product": "threading_building_blocks",
+        "version": "2021.11",
+        "version_strings": ["oneTBB: VERSION         2021.11"],
+    },
+]
+package_test_data = [
+    {
+        "url": "http://de.archive.ubuntu.com/ubuntu/pool/universe/o/onetbb/",
+        "package_name": "libtbb12_2021.12.0-1ubuntu2_amd64.deb",
+        "product": "threading_building_blocks",
+        "version": "2021.12",
+    },
+]
